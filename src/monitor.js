12acc15ea102fcb9adf46976e2351d477bfb17bb
Logan Saether <x@logansaether.com>
2020-02-06 11:06:34 -0700

    Add the node monitor

---
+++ src/monitor.js
@@ -0,0 +1,68 @@
+const { Octokit } = require('@octokit/rest');
+const semver = require('semver');
+
+class NodeMonitor {
+  constructor(storage) {
+    this.storage = storage;
+    this.ghApi = new Octokit();
+  }
+
+  /// Checks uptime and upgrade requirements.
+  async getLatestTag() {
+    const tags = await this.ghApi.repos.listTags({
+      owner: 'paritytech',
+      repo: 'polkadot'
+    });
+    const release = await this.ghApi.repos.getReleaseByTag({
+      owner: 'paritytech',
+      repo: 'polkadot',
+      tag: tags.data[0].name,
+    });
+    const publishedAt = new Date(release.data.published_at).getTime();
+    this.latestVersion = {
+      name: tags.data[0].name,
+      publishedAt,
+    };
+  }
+
+  async checkNodesVersion() {
+    const now = new Date().getTime();
+    const nodes = this.storage.getNodes();
+    // console.log(nodes);
+    for (const node of nodes) {
+      const version = semver.coerce(node.nodeDetails[2]);
+      const latestVersion = semver.clean(this.latestVersion.name);
+
+      const isUpgraded = semver.gte(version, latestVersion);
+
+      if (!isUpgraded) {
+        // TODO change this constant to a config value.
+        if (now > this.latestVersion.publishedAt + 1200) {
+          /// The node is out of date.
+          this.storage.updateNode(
+            node.id,
+            node.nodeDetails,
+            0,
+            0,
+            0,
+            0,
+          );
+        }
+      } else {
+        /// The node is doing good! Let's check if we should change its status back.
+        if (node.goodSince < 2) {
+          this.storage.updateNode(
+            node.id,
+            node.nodeDetails,
+            0,
+            0,
+            now,
+            0
+          );
+        }
+      }
+    }
+  }
+}
+
+module.exports = NodeMonitor;

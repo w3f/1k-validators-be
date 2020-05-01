64115aaa7bf7711d66c35fbaef3d171a6c519d19
Logan Saether <X@logansaether.com>
2020-05-01 12:59:07 +0200

    Bump

--- src/index.ts
+++ src/index.ts
@@ -169,5 +169,5 @@ program
   .option("--config <directory>", "The path to the config directory.", "config")
   .action((cmd: Command) => catchAndQuit(start(cmd)));
 
-program.version("1.2.1");
+program.version("1.2.2");
 program.parse(process.argv);

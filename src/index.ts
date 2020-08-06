5fe6407954c298d396db6a8479538054099b7717
Logan Saether <X@logansaether.com>
2020-08-06 03:42:27 -0700

    Upgrade dependencies

--- src/index.ts
+++ src/index.ts
@@ -167,5 +167,5 @@ program
   .option("--config <directory>", "The path to the config directory.", "config")
   .action((cmd: Command) => catchAndQuit(start(cmd)));
 
-program.version("1.2.26");
+program.version("1.2.27");
 program.parse(process.argv);

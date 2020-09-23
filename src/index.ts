19a12e65173d1c006a04d3b070039f79d9371e33
Logan Saether <X@logansaether.com>
2020-09-23 10:21:38 -0700

    Bump version to 1.4.1

--- src/index.ts
+++ src/index.ts
@@ -162,5 +162,5 @@ program
   .option("--config <directory>", "The path to the config directory.", "config")
   .action((cmd: Command) => catchAndQuit(start(cmd)));
 
-program.version("1.4.0");
+program.version("1.4.1");
 program.parse(process.argv);

c340c9eaefdbea83d7618d0c7ca3e9fe37ad076a
Logan Saether <X@logansaether.com>
2020-09-29 14:22:03 -0700

    Bump

--- src/monitor.ts
+++ src/monitor.ts
@@ -93,3 +93,11 @@ export default class Monitor {
     }
   }
 }
+
+// const test = async () => {
+//   const monitor = new Monitor({} as any, 0);
+//   const latestRelease = await monitor.getLatestTaggedRelease();
+//   console.log(latestRelease);
+// };
+
+// test();

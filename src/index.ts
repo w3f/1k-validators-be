e3f871c1507a023abb95e1e7409fe29051b6a603
wpank <9498646+wpank@users.noreply.github.com>
2020-11-02 11:06:00 +0100

    fix quote

--- src/index.ts
+++ src/index.ts
@@ -125,5 +125,5 @@ program
   .option("--config <directory>", "The path to the config directory.", "config")
   .action((cmd: Command) => catchAndQuit(start(cmd)));
 
-program.version("1.4.23");
+program.version("1.4.24");
 program.parse(process.argv);

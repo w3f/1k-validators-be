850e0656be14ff0889b2305a3fe02f5472883b31
Logan Saether <X@logansaether.com>
2020-11-03 16:55:12 +0100

    Fix tests

--- test/mock/index.ts
+++ test/mock/index.ts
@@ -4,9 +4,13 @@ export const MockApi = {
       era: 0,
     },
   },
+  getApi: () => MockApi,
   on: () => {},
   query: {
     staking: {
+      ledger: () => ({
+        unwrap: () => true,
+      }),
       currentEra: () => {
         return MockApi.mock.staking.era;
       },
@@ -102,57 +106,58 @@ export const MockConfig = {
   },
 };
 
-export const MockDb = {
-  allNodes: () => {
-    return [
-      {
-        name: "Alice",
-        stash: "5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY",
-        offlineSince: 0,
-        onlineSince: new Date().getTime() - 8 * 24 * 60 * 60 * 1000,
-        offlineAccumulated: 0,
-        connectedAt: 0,
-        nominatedAt: 1,
-      },
-      {
-        name: "Bob",
-        stash: "5HpG9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsSWFc",
-        offlineSince: 0,
-        onlineSince: new Date().getTime() - 8 * 24 * 60 * 60 * 1000,
-        offlineAccumulated: 0,
-        connectedAt: 1,
-        nominatedAt: 0,
-      },
-      {
-        name: "Charlie",
-        stash: null, // Filters because no stash.
-      },
-      {
-        name: "Dave",
-        stash: "5HpG9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsSWFc",
-        offlineSince: 100, // filter because offlineSince > 0
-      },
-      {
-        name: "Eve",
-        stash: "5HpG9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsSWFc",
-        offlineSince: 0,
-        onlineSince: new Date().getTime(), // filtered because not good for a week
-      },
-      {
-        name: "Ferdie",
-        stash: "5HpG9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsSWFc",
-        offlineSince: 0,
-        onlineSince: new Date().getTime() - 8 * 24 * 60 * 60 * 1000,
-        offlineAccumulated: 0.021 * 7 * 24 * 60 * 60 * 1000, // filtered due to too much
-      },
-      {
-        name: "George",
-        stash: "4HpG9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsSWFc", // doesn't have commission set right
-        offlineSince: 0,
-        onlineSince: new Date().getTime() - 8 * 24 * 60 * 60 * 1000,
-        offlineAccumulated: 0,
-      },
-    ];
-  },
-  getCurrentTargets: () => [],
-};
+// export const MockDb = {
+//   allNodes: () => {
+//     return [
+//       {
+//         name: "Alice",
+//         stash: "5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY",
+//         offlineSince: 0,
+//         onlineSince: new Date().getTime() - 8 * 24 * 60 * 60 * 1000,
+//         offlineAccumulated: 0,
+//         connectedAt: 0,
+//         nominatedAt: 1,
+//       },
+//       {
+//         name: "Bob",
+//         stash: "5HpG9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsSWFc",
+//         offlineSince: 0,
+//         onlineSince: new Date().getTime() - 8 * 24 * 60 * 60 * 1000,
+//         offlineAccumulated: 0,
+//         connectedAt: 1,
+//         nominatedAt: 0,
+//       },
+//       {
+//         name: "Charlie",
+//         stash: null, // Filters because no stash.
+//       },
+//       {
+//         name: "Dave",
+//         stash: "5HpG9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsSWFc",
+//         offlineSince: 100, // filter because offlineSince > 0
+//       },
+//       {
+//         name: "Eve",
+//         stash: "5HpG9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsSWFc",
+//         offlineSince: 0,
+//         onlineSince: new Date().getTime(), // filtered because not good for a week
+//       },
+//       {
+//         name: "Ferdie",
+//         stash: "5HpG9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsSWFc",
+//         offlineSince: 0,
+//         onlineSince: new Date().getTime() - 8 * 24 * 60 * 60 * 1000,
+//         offlineAccumulated: 0.021 * 7 * 24 * 60 * 60 * 1000, // filtered due to too much
+//       },
+//       {
+//         name: "George",
+//         stash: "4HpG9w8EBLe5XCrbczpwq5TSXvedjrBGCwqxK1iQ7qUsSWFc", // doesn't have commission set right
+//         offlineSince: 0,
+//         onlineSince: new Date().getTime() - 8 * 24 * 60 * 60 * 1000,
+//         offlineAccumulated: 0,
+//       },
+//     ];
+//   },
+//   getCurrentTargets: () => [],
+//   newAccountingRecord: () => true,
+// };

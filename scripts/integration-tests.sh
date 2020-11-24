5850d8b39ad4f3898832d11c6a0c242f2caab59d
pantelis <pampatzoglou@gmail.com>
2020-11-24 14:24:25 +0200

    move namespace to helmfile.d and update integration test

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -7,17 +7,30 @@ source /scripts/bootstrap-helm.sh
 run_tests() {
     echo Running tests...
 
-    wait_pod_ready otv-backend-0
+    wait_pod_ready mongodb-replicaset-0 kusama
+    wait_pod_ready mongodb-replicaset-1 kusama
+    wait_pod_ready otv-backend-0 kusama
 
-    express_pod=$(kubectl get pods | grep express | cut -d' ' -f1)
-    wait_pod_ready $express_pod
+    express_pod=$(kubectl -n kusama get pods | grep express | cut -d' ' -f1)
+    wait_pod_ready $express_pod kusama
+
+    wait_pod_ready mongodb-replicaset-0 polkadot
+    wait_pod_ready mongodb-replicaset-1 polkadot
+    wait_pod_ready otv-backend-0 polkadot
+
+    express_pod=$(kubectl -n polkadot get pods | grep express | cut -d' ' -f1)
+    wait_pod_ready $express_pod polkadot    
 }
 
 teardown() {
     retval=$?
     if [ $retval -ne 0 ]; then
-        kubectl describe pod otv-backend-0
-        kubectl logs -l app=otv-backend
+        kubectl -n kusama describe pod otv-backend-0 
+        kubectl -n kusama logs -l app=otv-backend
+
+        kubectl -n polkadot describe pod otv-backend-0 
+        kubectl -n polkadot logs -l app=otv-backend
+
     fi
 
     helmfile delete --purge

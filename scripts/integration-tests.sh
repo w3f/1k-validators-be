a9b9c85ca2e929ba10e62687f5bf07c033cace68
pantelis <pampatzoglou@gmail.com>
2020-11-26 18:11:58 +0200

    reduce replicas in ci

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -14,7 +14,7 @@ run_tests() {
 
     express_pod=$(kubectl -n kusama get pods | grep express | cut -d' ' -f1)
     wait_pod_ready $express_pod kusama 1/1
-
+    kubectl delete namespace kusama
 
     wait_pod_ready polkadot-mongodb-0 polkadot 2/2
     wait_pod_ready polkadot-mongodb-1 polkadot 2/2

c5aa24bbc07330d78a145c8c63d7dfde0456bc84
Logan Saether <X@logansaether.com>
2020-12-01 10:47:55 +0100

    Use mongoose as any

--- src/db/index.ts
+++ src/db/index.ts
@@ -12,8 +12,7 @@ import logger from "../logger";
 export type NodeDetails = [string, string, string, string, string];
 
 // Sets a global configuration to silence mongoose deprecation warnings.
-//@ts-ignore
-mongoose.set("useFindAndModify", false);
+(mongoose as any).set("useFindAndModify", false);
 
 export default class Db {
   private accountingModel;

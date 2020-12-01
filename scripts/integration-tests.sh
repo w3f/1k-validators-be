f1be236d962c725ca9b3acb93c945f785ae78543
pantelis <pampatzoglou@gmail.com>
2020-12-01 13:08:57 +0200

    reduce load on ci

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -8,27 +8,26 @@ run_tests() {
     echo Running tests...
 
     wait_pod_ready kusama-mongodb-0 kusama 2/2
-    wait_pod_ready kusama-mongodb-arbiter-0 kusama 1/1
     wait_pod_ready kusama-otv-backend-0 kusama 1/1
 
-    express_pod=$(kubectl -n kusama get pods | grep express | cut -d' ' -f1)
-    wait_pod_ready $express_pod kusama 1/1
+    # express_pod=$(kubectl -n kusama get pods | grep express | cut -d' ' -f1)
+    # wait_pod_ready $express_pod kusama 1/1
     kubectl delete namespace kusama
 
     wait_pod_ready polkadot-mongodb-0 polkadot 2/2
-    wait_pod_ready polkadot-mongodb-arbiter-0 polkadot 1/1
     wait_pod_ready polkadot-otv-backend-0 polkadot 1/1
 
-    express_pod=$(kubectl -n polkadot get pods | grep express | cut -d' ' -f1)
-    wait_pod_ready $express_pod polkadot 1/1    
+    # express_pod=$(kubectl -n polkadot get pods | grep express | cut -d' ' -f1)
+    # wait_pod_ready $express_pod polkadot 1/1    
 }
 
 teardown() {
     retval=$?
     if [ $retval -ne 0 ]; then
-        kubectl describe pod otv-backend-0 
-        kubectl logs -l app=otv-backend
-
+        kubectl -n kusama describe pod kusama-otv-backend-0 
+        kubectl -n kusama logs -l app=kusama-otv-backend
+        kubectl -n polkadot describe pod polkadot-otv-backend-0 
+        kubectl -n polkadot logs -l app=polkadot-otv-backend
     fi
 
     helmfile delete --purge

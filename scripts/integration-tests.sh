5e15f3c0389e9bdc4baec2650e9a1739d36ed79b
pampatzoglou <15629182+pampatzoglou@users.noreply.github.com>
2020-12-02 12:49:47 +0200

    WIP: Debug (#532)
    * fix arbiter
    
    * include replicaset in backend connector
    
    * better json handling
    
    * test
    
    * test
    
    * test
    
    * remove prettyjson
    
    * bump

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -14,8 +14,8 @@ run_tests() {
     # wait_pod_ready $express_pod kusama 1/1
     kubectl delete namespace kusama
 
-    wait_pod_ready polkadot-mongodb-0 polkadot 2/2
-    wait_pod_ready polkadot-otv-backend-0 polkadot 1/1
+    #wait_pod_ready polkadot-mongodb-0 polkadot 2/2
+    #wait_pod_ready polkadot-otv-backend-0 polkadot 1/1
 
     # express_pod=$(kubectl -n polkadot get pods | grep express | cut -d' ' -f1)
     # wait_pod_ready $express_pod polkadot 1/1    
@@ -26,6 +26,9 @@ teardown() {
     if [ $retval -ne 0 ]; then
         kubectl -n kusama describe pod kusama-otv-backend-0 
         kubectl -n kusama logs -l app=kusama-otv-backend
+
+
+
         kubectl -n polkadot describe pod polkadot-otv-backend-0 
         kubectl -n polkadot logs -l app=polkadot-otv-backend
     fi

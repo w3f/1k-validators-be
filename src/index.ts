74c17f7011042b4408c9ab498e554f497efa7b3f
wpank <9498646+wpank@users.noreply.github.com>
2020-12-15 10:47:15 +0100

    bump version to 2.1.12

--- src/index.ts
+++ src/index.ts
@@ -130,5 +130,5 @@ program
   .option("--config <directory>", "The path to the config directory.", "config")
   .action((cmd: Command) => catchAndQuit(start(cmd)));
 
-program.version("2.1.10");
+program.version("2.1.12");
 program.parse(process.argv);

762a4d7c5aec5a7097fb877f261bcb5cf3327974
Logan Saether <X@logansaether.com>
2021-01-05 12:07:08 +0100

    Comment out retroactive entirely

--- src/index.ts
+++ src/index.ts
@@ -124,11 +124,11 @@ const start = async (cmd: Command) => {
   sleep(3000);
 
   // Start the scorekeeper
-  if (config.global.retroactive && !process.env.CI) {
-    retroactiveRanks(config.scorekeeper.candidates as any, handler, db);
-  } else {
-    scorekeeper.begin();
-  }
+  // if (config.global.retroactive && !process.env.CI) {
+  //   retroactiveRanks(config.scorekeeper.candidates as any, handler, db);
+  // } else {
+  scorekeeper.begin();
+  // }
 
   // Start the API server.
   const server = new Server(db, config, scorekeeper);

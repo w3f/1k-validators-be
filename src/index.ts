7744444f784cde3c682046bd5660b1b90894bcb5
wpank <9498646+wpank@users.noreply.github.com>
2021-01-27 15:45:03 +0100

    bump version to 2.2.28

--- src/index.ts
+++ src/index.ts
@@ -141,5 +141,5 @@ program
   .option("--config <directory>", "The path to the config directory.", "config")
   .action((cmd: { config: string }) => catchAndQuit(start(cmd)));
 
-program.version("2.2.27");
+program.version("2.2.28");
 program.parse(process.argv);

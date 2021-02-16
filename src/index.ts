aef57e9844cb9a68a2d87ebdef8c9e48dcb7df2e
wpank <will.pankewicz@gmail.com>
2021-02-16 12:14:13 +0100

    start scorekeeper

--- src/index.ts
+++ src/index.ts
@@ -128,9 +128,9 @@ const start = async (cmd: { config: string }) => {
 
   // Start the scorekeeper
   // if (config.global.retroactive && !isCI) {
-  retroactiveRanks(config.scorekeeper.candidates as any, handler, db);
+  //  retroactiveRanks(config.scorekeeper.candidates as any, handler, db);
   // } else {
-    // scorekeeper.begin();
+  scorekeeper.begin();
   // }
 
   // Start the API server.

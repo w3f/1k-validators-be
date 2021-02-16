0d12d48bedc6915ab5a80ee83ff87f20ce24d1f8
wpank <will.pankewicz@gmail.com>
2021-02-16 12:08:37 +0100

    bump version to v2.2.40

--- src/index.ts
+++ src/index.ts
@@ -142,5 +142,5 @@ program
   .option("--config <directory>", "The path to the config directory.", "config")
   .action((cmd: { config: string }) => catchAndQuit(start(cmd)));
 
-program.version("2.2.39");
+program.version("2.2.40");
 program.parse(process.argv);

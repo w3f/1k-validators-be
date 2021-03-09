dcfc6a976525ad5f4afafb91974b12b799f6009d
wpank <9498646+wpank@users.noreply.github.com>
2021-03-09 10:07:31 +0100

    test out ci logging changes

--- src/index.ts
+++ src/index.ts
@@ -88,7 +88,7 @@ const start = async (cmd: { config: string }) => {
 
   // Create the matrix bot if enabled.
   let maybeBot: any = false;
-  if (config.matrix.enabled && !isCI) {
+  if (config.matrix.enabled) {
     const { accessToken, baseUrl, userId } = config.matrix;
     maybeBot = new MatrixBot(baseUrl, accessToken, userId, db, config);
     maybeBot.start();

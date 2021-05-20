ec24a38299f878519c11ae2ad8fdbb4df16839a7
lamafab <42901763+lamafab@users.noreply.github.com>
2021-05-20 15:23:32 +0200

    bump version

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ import { startClearAccumulatedOfflineTimeJob, startMonitorJob } from "./cron";
 
 const isCI = process.env.CI;
 
-const version = "v2.3.97";
+const version = "v2.3.98";
 
 const catchAndQuit = async (fn: any) => {
   try {

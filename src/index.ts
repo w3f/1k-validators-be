a82db93752cb71d7643f5bca3bab9ec7324322d0
wpank <9498646+wpank@users.noreply.github.com>
2021-07-13 11:16:12 +0200

    add more kusama vals

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ import { startClearAccumulatedOfflineTimeJob, startMonitorJob } from "./cron";
 
 const isCI = process.env.CI;
 
-const version = "v2.4.43";
+const version = "v2.4.44";
 
 const catchAndQuit = async (fn: any) => {
   try {

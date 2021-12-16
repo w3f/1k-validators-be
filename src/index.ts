642a3c00119801586f6945fab3c540fd69000436
wpank <9498646+wpank@users.noreply.github.com>
2021-12-16 12:54:57 +0100

    bump version to v2.5.4

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ import { startClearAccumulatedOfflineTimeJob, startMonitorJob } from "./cron";
 
 const isCI = process.env.CI;
 
-const version = "v2.5.3";
+const version = "v2.5.4";
 
 const catchAndQuit = async (fn: any) => {
   try {

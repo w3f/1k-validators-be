e352f8547e5a962c4428365208c06ad7cfef93d0
wpank <9498646+wpank@users.noreply.github.com>
2022-01-11 17:25:40 +0100

    bump version to v2.5.55

--- src/scorekeeper.ts
+++ src/scorekeeper.ts
@@ -375,7 +375,7 @@ export default class ScoreKeeper {
         continue;
       } else {
         const stash = await nom.stash();
-        const payee = await nom.payee();
+        // const payee = await nom.payee();
         const [bonded, err] = await this.chaindata.getBondedAmount(stash);
         const proxy = nom.isProxy ? nom.address : "";
         const proxyDelay = nom.proxyDelay;

aad5a42f4e7e12a5674d0d06c3f6d8f284012dbd
Tommi <tommi@niemi.lol>
2022-01-24 06:41:21 +0000

    add kusamaxi 2nd val and bump version

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ import { startClearAccumulatedOfflineTimeJob, startMonitorJob } from "./cron";
 
 const isCI = process.env.CI;
 
-const version = "v2.5.68";
+const version = "v2.5.69";
 
 const catchAndQuit = async (fn: any) => {
   try {

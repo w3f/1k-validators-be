bb20bc0bfd0495d693f2f45cec88229cd8736cf2
wpank <9498646+wpank@users.noreply.github.com>
2022-09-02 12:30:56 +0200

    separate out telemetry into its own package

---
+++ packages/common/src/ApiHandler.ts
@@ -0,0 +1,143 @@
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import EventEmitter from "eventemitter3";
+
+import logger from "./logger";
+import { sleep } from "./util";
+import { POLKADOT_API_TIMEOUT } from "./constants";
+
+/**
+ * A higher level handler for the Polkadot-Js API that can handle reconnecting
+ * to a different provider if one proves troublesome.
+ */
+class ApiHandler extends EventEmitter {
+  private _api: ApiPromise;
+  private _endpoints: string[];
+  private _reconnectLock: boolean;
+  private _reconnectTries = 0;
+  static isConnected: any;
+  static _reconnect: any;
+
+  constructor(api: ApiPromise, endpoints: string[]) {
+    super();
+    this._api = api;
+    this._endpoints = endpoints;
+    this._registerEventHandlers(api);
+  }
+
+  static async createApi(endpoints) {
+    const api = new ApiPromise({
+      provider: new WsProvider(
+        endpoints,
+        undefined,
+        undefined,
+        POLKADOT_API_TIMEOUT
+      ),
+      // throwOnConnect: true,
+    });
+    if (api) {
+      api
+        .on("connected", () => {
+          logger.info(`Connected to chain`);
+        })
+        .on("disconnected", async () => {
+          logger.warn(`Disconnected from chain`);
+        })
+        .on("ready", () => {
+          logger.info(`API connection ready`);
+        })
+        .on("error", (error) => {
+          logger.warn("The API has an error");
+          console.log(error);
+        });
+      await api.isReadyOrError;
+      return api;
+    }
+  }
+
+  static async create(endpoints: string[]): Promise<ApiHandler> {
+    try {
+      const api = await this.createApi(endpoints);
+      return new ApiHandler(api, endpoints);
+    } catch (e) {
+      logger.info(`there was an error: `);
+      console.log(e);
+    }
+  }
+
+  isConnected(): boolean {
+    return this._api.isConnected;
+  }
+
+  async getApi(): Promise<ApiPromise> {
+    if (this._reconnectLock) {
+      return new Promise((resolve) => {
+        setTimeout(() => resolve(this.getApi()), 2000);
+      });
+    }
+
+    return this._api;
+  }
+
+  _registerEventHandlers(api: ApiPromise): void {
+    api.query.system.events((events) => {
+      // Loop through the Vec<EventRecord>
+      events.forEach((record) => {
+        // Extract the phase, event and the event types
+        const { event } = record;
+
+        if (event.section == "session" && event.method == "NewSession") {
+          const [session_index] = event.data;
+
+          this.emit("newSession", {
+            sessionIndex: session_index.toString(),
+          });
+        }
+
+        if (
+          event.section == "staking" &&
+          (event.method == "Reward" || event.method == "Rewarded")
+        ) {
+          const [stash, amount] = event.data;
+
+          this.emit("reward", {
+            stash: stash.toString(),
+            amount: amount.toString(),
+          });
+        }
+
+        if (event.section === "imOnline" && event.method === "SomeOffline") {
+          const offlineVals = event.data.toJSON()[0].map((val) => val[0]);
+
+          this.emit("someOffline", {
+            offlineVals: offlineVals,
+          });
+        }
+      });
+    });
+  }
+
+  async _reconnect(): Promise<void> {
+    if (this._reconnectLock) {
+      logger.info(`API Already Trying Reconnect...`);
+      return;
+    }
+
+    logger.info(
+      `API Disconnected... Reconnecting... (reconnect tries: ${this._reconnectTries})`
+    );
+    this._reconnectLock = true;
+    this._reconnectTries++;
+    // disconnect from the old one
+    this._api.disconnect();
+    // do the actual reconnection
+    const nextEndpoint = this._endpoints[this._reconnectTries % 5];
+    const api = await ApiPromise.create({
+      provider: new WsProvider(nextEndpoint),
+    });
+    this._registerEventHandlers(api);
+    this._api = api;
+    this._reconnectLock = false;
+  }
+}
+
+export default ApiHandler;
---
+++ packages/telemetry/src/telemetry.ts
@@ -0,0 +1,194 @@
+import ReconnectingWebSocket from "reconnecting-websocket";
+import WS from "ws";
+
+import { Db, Config, logger } from "@1kv/common";
+
+enum TelemetryMessage {
+    FeedVersion = 0x00,
+    BestBlock = 0x01,
+    BestFinalized = 0x02,
+    AddedNode = 0x03,
+    RemovedNode = 0x04,
+    LocatedNode = 0x05,
+    ImportedBlock = 0x06,
+    FinalizedBlock = 0x07,
+    NodeStats = 0x08,
+    NodeHardware = 0x09,
+    TimeSync = 0x10,
+}
+
+const DEFAULT_HOST = "ws://localhost:8000/feed";
+
+const MemNodes = {};
+
+export default class TelemetryClient {
+    private config: Config.ConfigSchema;
+    private db: Db;
+    private host: string;
+    private socket: ReconnectingWebSocket;
+    // map of name -> boolean
+    private beingReported: Map<string, boolean> = new Map();
+
+    private offlineNodes: Map<number, boolean> = new Map();
+
+    constructor(config: Config.ConfigSchema, db: Db) {
+        this.config = config;
+        this.db = db;
+        this.host = this.config.telemetry.host || DEFAULT_HOST;
+
+        const options = {
+            WebSocket: WS,
+            connectionTimeout: 10000,
+            maxRetries: 15,
+        };
+
+        this.socket = new ReconnectingWebSocket(this.host, [], options);
+    }
+
+    async start(): Promise<any> {
+        return new Promise((resolve: any, reject: any) => {
+            this.socket.onopen = () => {
+                logger.info(`Connected to substrate-telemetry on host ${this.host}`);
+                for (const chain of this.config.telemetry.chains) {
+                    this._subscribe(chain);
+                }
+                resolve();
+            };
+
+            this.socket.onclose = () => {
+                logger.info(
+                    `Connection to substrate-telemetry on host ${this.host} closed`
+                );
+                reject();
+            };
+
+            this.socket.onerror = (err: any) => {
+                logger.info(
+                    `Could not connect to substrate-telemetry on host ${this.host}: `
+                );
+                logger.info(err);
+                reject();
+            };
+
+            this.socket.onmessage = (msg: any) => {
+                const messages = this._deserialize(msg);
+                for (const message of messages) {
+                    this._handle(message);
+                }
+            };
+        });
+    }
+
+    private _deserialize(msg: any) {
+        const data = JSON.parse(msg.data);
+        const messages = new Array(data.length / 2);
+
+        for (const index of messages.keys()) {
+            const [action, payload] = data.slice(index * 2);
+            // eslint-disable-next-line security/detect-object-injection
+            messages[index] = { action, payload };
+        }
+
+        return messages;
+    }
+
+    private async _handle(message: any) {
+        const { action, payload } = message;
+
+        switch (action) {
+            case TelemetryMessage.FeedVersion:
+            {
+                logger.info(`feed version: ${JSON.stringify(payload)}`);
+            }
+                break;
+            case TelemetryMessage.AddedNode:
+            {
+                const [
+                    id,
+                    details,
+                    nodeStats,
+                    nodeIO,
+                    nodeHardware,
+                    blockDetails,
+                    location,
+                    startupTime,
+                ] = payload;
+                const [lat, lon, city] = location || ["", "", "No Location"];
+                const now = Date.now();
+
+                MemNodes[parseInt(id)] = details;
+
+                // a mutex that will only update after its free to avoid race conditions
+                const waitUntilFree = async (name: string): Promise<void> => {
+                    if (this.beingReported.get(name)) {
+                        return new Promise((resolve) => {
+                            const intervalId = setInterval(() => {
+                                if (!this.beingReported.get(name)) {
+                                    clearInterval(intervalId);
+                                    resolve();
+                                }
+                            }, 1000);
+                        });
+                    }
+                };
+
+                await waitUntilFree(details[0]);
+                await this.db.reportOnline(id, details, now, city);
+
+                const wasOffline = this.offlineNodes.has(id);
+                if (wasOffline) {
+                    this.offlineNodes.delete(id);
+                }
+            }
+                break;
+            case TelemetryMessage.RemovedNode:
+            {
+                const id = parseInt(payload);
+                const now = Date.now();
+
+                //this is to get around security warning vvv
+                const details = MemNodes[parseInt(String(id))];
+
+                if (!details) {
+                    logger.info(`Unknown node with ${id} reported offline.`);
+                }
+
+                const name = details[0];
+
+                logger.info(`(TELEMETRY) Reporting ${name} OFFLINE`);
+                this.beingReported.set(name, true);
+                await this.db.reportOffline(id, name, now);
+                this.beingReported.set(name, false);
+
+                this.offlineNodes.set(id, true);
+            }
+                break;
+            case TelemetryMessage.LocatedNode:
+            {
+                const [id, lat, lon, city] = payload;
+                // await this.db.setLocation(id, city);
+            }
+                break;
+            case TelemetryMessage.ImportedBlock:
+            {
+                const [id, details] = payload;
+                const now = Date.now();
+
+                const wasOffline = this.offlineNodes.has(id);
+                if (wasOffline) {
+                    this.offlineNodes.delete(id);
+                    await this.db.reportBestBlock(id, details, now);
+                }
+            }
+                break;
+        }
+    }
+
+    private async _subscribe(chain: string, finality = true) {
+        if (this.config.telemetry.chains.includes(chain)) {
+            this.socket.send(`ping:${chain}`);
+            this.socket.send(`subscribe:${chain}`);
+            logger.info(`Subscribed to ${chain}`);
+        }
+    }
+}

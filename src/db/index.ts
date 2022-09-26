5d8c16cd2b0f0e93f470c00b2b333590538a99df
wpank <9498646+wpank@users.noreply.github.com>
2022-09-26 13:51:38 +0200

    bump version to v2.6.54

--- src/db/index.ts
+++ src/db/index.ts
@@ -2915,17 +2915,16 @@ export default class Db {
     // First try to get by telemetry name
     data = await this.locationModel
       .findOne({
-        name,
         addr,
       })
       .exec();
-    if (!data) {
-      data = await this.locationModel
-        .findOne({
-          addr,
-        })
-        .exec();
-    }
+    // if (!data) {
+    //   data = await this.locationModel
+    //     .findOne({
+    //       addr,
+    //     })
+    //     .exec();
+    // }
     return data;
   }
 

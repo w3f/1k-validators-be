85703cfcb96a324a09fe675a537b355d74946ee3
wpank <9498646+wpank@users.noreply.github.com>
2022-10-17 01:40:32 +0200

    add worker

---
+++ packages/worker/src/run.ts
@@ -0,0 +1,32 @@
+import Worker from "./worker";
+import { Command } from "commander";
+import { Db, Config, logger } from "@1kv/common";
+
+const version = "v3.0.0";
+
+const catchAndQuit = async (fn: any) => {
+  try {
+    await fn;
+  } catch (e) {
+    console.error(e.toString());
+    process.exit(1);
+  }
+};
+
+const start = async (cmd: { config: string }) => {
+  const config = Config.loadConfigDir(cmd.config);
+
+  logger.info(`{Worker:Start} Starting the backend services. ${version}`);
+  const db = await Db.create(config.db.mongo.uri);
+  const worker = new Worker(db, config);
+  await worker.startWorker();
+};
+
+const program = new Command();
+
+program
+  .option("--config <directory>", "The path to the config directory.", "config")
+  .action((cmd: { config: string }) => catchAndQuit(start(cmd)));
+
+program.version(version);
+program.parse(process.argv);

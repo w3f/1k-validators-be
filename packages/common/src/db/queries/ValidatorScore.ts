056ab1378874fb69ee1ddb7fc2edafa74b090ffb
will pankiewicz <will@wills-MacBook-Pro-2.local>
2023-03-03 13:02:07 -0700

    bump version to v2.7.98

--- packages/common/src/db/queries/ValidatorScore.ts
+++ packages/common/src/db/queries/ValidatorScore.ts
@@ -128,8 +128,7 @@ export const deleteOldValidatorScores = async (): Promise<any> => {
   const FIVE_MINUTES = 300000;
   const ONE_WEEK = 604800016.56;
   const ONE_MONTH = 2629800000;
-  const timeWindow = Date.now() - FIVE_MINUTES;
-  logger.info(``);
+  const timeWindow = Date.now() - ONE_WEEK;
   const scoreToDelete = await ValidatorScoreModel.find({
     updated: { $lt: timeWindow },
   })

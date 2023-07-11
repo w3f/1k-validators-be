9096c75e792e38a97fa2dacf3f124e5fdb48afce
will pankiewicz <will@wills-MacBook-Pro-2.local>
2023-07-11 15:57:38 +0200

    bump version to v2.8.48

--- packages/worker/src/jobs/DelegationJob.ts
+++ packages/worker/src/jobs/DelegationJob.ts
@@ -5,12 +5,12 @@ export const delegationLabel = { label: "DelegationJob" };
 export const delegationJob = async (chaindata: ChainData) => {
   const start = Date.now();
 
-  const chainType = await chaindata.getChainType();
-
   try {
     const delegations = await chaindata.getOpenGovDelegations();
     await queries.wipeOpenGovDelegations();
+    logger.info(`Adding ${delegations.length} delegations`, delegationLabel);
     for (const delegation of delegations) {
+      logger.info(`Adding delegation ${delegation.delegator}`, delegationLabel);
       await queries.addOpenGovDelegation(delegation);
     }
   } catch (e) {
@@ -26,4 +26,5 @@ export const delegationJob = async (chaindata: ChainData) => {
 export const processDelegationJob = async (job: any, chaindata: ChainData) => {
   logger.info(`Processing Delegation Job....`, delegationLabel);
   await delegationJob(chaindata);
+  logger.info(`Delegation Job Processed!`, delegationLabel);
 };

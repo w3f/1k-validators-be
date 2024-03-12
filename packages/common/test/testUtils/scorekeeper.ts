c5f463b543551ef007bde96ea65b39a56a053b25
will pankiewicz <will@wills-MacBook-Pro-2.local>
2024-03-12 02:57:00 +0100

    fix lint

--- packages/common/test/testUtils/scorekeeper.ts
+++ packages/common/test/testUtils/scorekeeper.ts
@@ -10,7 +10,6 @@ export const getAndStartScorekeeper = async () => {
   await apiHandler.getApi()?.isReady;
   let health = await apiHandler.healthCheck();
   while (!health) {
-    console.log("API is not ready, retrying in 5 seconds");
     await new Promise((resolve) => setTimeout(resolve, 5000));
     health = await apiHandler.healthCheck();
   }
@@ -18,25 +17,16 @@ export const getAndStartScorekeeper = async () => {
   await addProdKusamaCandidates();
 
   const config = getKusamaProdConfig();
-  console.log("got config");
-
-  console.log(JSON.stringify(config));
 
   const scorekeeper = new ScoreKeeper(apiHandler, config);
   for (const nominatorGroup of config.scorekeeper.nominators) {
     await scorekeeper.addNominatorGroup(nominatorGroup);
   }
 
-  console.log(`nominator groups added beginning scorekeeper....`);
   const didStart = await scorekeeper.begin();
-  console.log("did start:");
-  console.log(didStart);
-  console.log("yes");
 
-  console.log("scorekeeper has begun");
   let isStarted = scorekeeper.isStarted;
   while (!isStarted) {
-    console.log("waiting for scorekeeper to start...");
     await new Promise((resolve) => setTimeout(resolve, 5000));
     isStarted = scorekeeper.isStarted;
   }
